5332d041e16c8080987bdfa79d2f824259face59
emilioCode <emilio_mem@hotmail.com>
2024-04-06 18:50:09 -0400

    subscription implementd to unsubscribe when the component get out

--- src/app/shared/pokemon/search/search.component.ts
+++ src/app/shared/pokemon/search/search.component.ts
@@ -1,39 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { InteractionService } from '../services/interaction.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'pokemon-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.css']
 })
-export class SearchComponent{
+export class SearchComponent implements OnDestroy {
 
-  variable:string = '';
-  pokemon:any= {};
+  variable: string = '';
+  pokemon: any = {};
   headers = new HttpHeaders().set("content-type", "application/json");
-  http:any;
-  url:string = 'https://pokeapi.co/api/v2/pokemon/';
-  interactionService:any;
+  http: any;
+  url: string = 'https://pokeapi.co/api/v2/pokemon/';
+  interactionService: any;
+  subscription!: Subscription;
 
-  constructor(private _http: HttpClient, private _interactionService:InteractionService){
+  constructor(private _http: HttpClient, private _interactionService: InteractionService) {
     this.http = _http;
     this.interactionService = _interactionService;
   }
 
-  getPokemon(value:string){
+  getPokemon(value: string) {
     this.interactionService.loading = true;
-    this.http.get(this.url + value,{headers:this.headers,responseType:'json'})
-    .subscribe((data:any)=>{
-      this.pokemon = {};
-      this.pokemon.id = data.id;
-      this.pokemon.name = data.name;
-      this.pokemon.source = data.sprites.front_default;
-      this.interactionService.loading = false;
-    },(error:any) => {
-      console.error(error);
-      this.pokemon = {};
-      this.interactionService.loading = false;
-    });
+    this.subscription = this.http.get(this.url + value, { headers: this.headers, responseType: 'json' })
+      .subscribe((data: any) => {
+        this.pokemon = {};
+        this.pokemon.id = data.id;
+        this.pokemon.name = data.name;
+        this.pokemon.source = data.sprites.front_default;
+        this.interactionService.loading = false;
+      }, (error: any) => {
+        console.error(error);
+        this.pokemon = {};
+        this.interactionService.loading = false;
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 }
